7fe391e7f5d660acbbe1764284efa38af89a970d
Guilherme Pereira <web.guilhermepereira@gmail.com>
2024-05-09 10:52:35 -0300

    feat: Resolução do desafio de contas a pagar

--- src/app/pages/accounts-payable/accounts-payable.component.ts
+++ src/app/pages/accounts-payable/accounts-payable.component.ts
@@ -3,6 +3,8 @@ import { BusinessService } from '../../shared/service/business.service';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 
 
@@ -12,27 +14,72 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrl: './accounts-payable.component.css'
 })
 export class AccountsPayableComponent implements OnInit {
-  displayedColumns: string[] = ['title', 'company', 'date', 'value'];
+
+  form: FormGroup = new FormGroup({});
+
+  accounts: Array<any> = [];
+  accountsMock: Array<any> = [];
+  accountsLocalStorage: Array<any> = [];
+  accountsRxjs: BehaviorSubject<any> = new BehaviorSubject<any>({});
+
   dataSource: MatTableDataSource<any> = new MatTableDataSource();
+  displayedColumns: string[] = ['title', 'company', 'date', 'value'];
 
-  @ViewChild(MatPaginator) paginator: any;
+
   @ViewChild(MatSort) sort: any;
+  @ViewChild(MatPaginator) paginator: any;
 
-  constructor(private businessService: BusinessService) {
+  constructor(private businessService: BusinessService, private fromBuilder: FormBuilder
+  ) {
+    this.createForm();
   }
 
 
   ngOnInit() {
-    this.getAccountsPayable();
-
+    this.getAccountsPayableLocalStorage();
   }
 
-
-  getAccountsPayable() {
-    this.businessService.getAccountsPayable().subscribe((res: any) => {
-      this.dataSource = new MatTableDataSource(res);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+  createForm() {
+    this.form = this.fromBuilder.group({
+      title: new FormControl('', [Validators.required]),
+      value: new FormControl('', [Validators.required]),
+      company: new FormControl('', [Validators.required]),
+      date: new FormControl('', [Validators.required]),
     })
+  }
+
+  getAccountsPayableLocalStorage() {
+    const accounts: any = localStorage.getItem('accountsPayable');
+    const data = accounts ? JSON.parse(accounts) : [];
+    this.accountsLocalStorage = data;
+    this.getAccountsPayableMOCK();
+  }
+
+  getAccountsPayableMOCK() {
+    this.businessService.getAccountsPayable().subscribe((res: any) => {
+      this.accountsMock = res;
+      this.accounts = this.accountsMock;
+      this.accountsLocalStorage.forEach(row => this.accounts.push(row))
+      this.setDataInTable();
+    })
+  }
+
+  setDataInTable() {
+    this.dataSource = new MatTableDataSource(this.accounts);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  save() {
+    const data = this.form.value;
+    this.accounts.push(data);
+    // SALVAMENTO COM LOCALSTORAGE
+    this.accountsLocalStorage.push(data);
+    localStorage.setItem('accountsPayable', JSON.stringify(this.accountsLocalStorage));
+    // SALVAMENTO COM RXJS
+    this.accountsRxjs.next(data);
+    // INCLUIR A CONTA NOVA NA TABELA
+    this.setDataInTable();
+    this.form.reset();
   }
 }
